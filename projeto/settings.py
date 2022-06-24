9ce02a1756a2581b206364a4e87cbd65fc64670f
WederTito <wedertito8@gmail.com>
2022-06-24 15:36:57 -0300

    Exemplo de templates

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Colocar os App aqui a baixo
+    'recipes'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,10 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Tito',
+    })
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
